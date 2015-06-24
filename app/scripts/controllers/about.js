7efdf60956d66aff1056c0785522c512bae3a270
Pako Navarro <packarbell@gmail.com>
2015-06-24 20:06:26 +0200

    Base de yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name outfitScreensApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the outfitScreensApp
+ */
+angular.module('outfitScreensApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name outfitScreensApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the outfitScreensApp
+ */
+angular.module('outfitScreensApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
